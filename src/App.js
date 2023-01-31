803e779cbdd34f84f9c629150f0a9919666c54be
ChristinaDo <109245422+ChristinaDo@users.noreply.github.com>
2023-01-31 22:14:42 +0100

    styled the application

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <div className="container">
         <header className="App-header"></header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="ocean" />
         </main>
         <footer className="App-footer">Coded by Christina Doneus</footer>
       </div>
